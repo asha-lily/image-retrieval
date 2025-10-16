62d406dfae3b24c7d01b071dca24e00f4185fd59
asha-lily <ashalilypatel@gmail.com>
2025-10-16 20:51:06 +0100

    Add pydantic validation.

--- local_rag/query_image_db.py
+++ local_rag/query_image_db.py
@@ -6,35 +6,73 @@ import chromadb
 from chromadb.utils.data_loaders import ImageLoader
 from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
 
+from pydantic_core import PydanticCustomError
+from pydantic import BaseModel, field_validator
+
+
 config = Config() 
-
-db_location = Path(config.vector_db_path)
-client = chromadb.PersistentClient(path=db_location)
 embedding_function = OpenCLIPEmbeddingFunction()
-data_loader = ImageLoader()
 
-def get_collection(collection_name: str) -> chromadb.Collection:
+
+class ValidateArgs(BaseModel):
+    db_location: Path
+    num_results_to_retrieve: int
+    collection_name: str
+
+    @field_validator("db_location")
+    def validate_db_exists(cls, value: Path):
+        if not value.exists():
+            raise PydanticCustomError(
+                "db_doesn't_exist_error",
+                "DB path doesn't exist.",
+                {"path": value}
+            )
+
+
+def load_args():
+    db_location = Path(config.vector_db_path)
+    num_results_to_retrieve = config.num_results_to_retrieve
+    collection_name = config.image_collection_name
+
+    ValidateArgs(
+        db_location=db_location,
+        num_results_to_retrieve=num_results_to_retrieve,
+        collection_name=collection_name,
+    )
+
+    return (
+        db_location, 
+        num_results_to_retrieve,
+        collection_name,
+    )
+
+
+def get_collection(collection_name: str, client: chromadb.Client) -> chromadb.Collection:
     collection = client.get_or_create_collection(
         name=collection_name,
         embedding_function=embedding_function,
-        data_loader=data_loader
+        data_loader=ImageLoader()
     )
     return collection
 
 
-def main():
+def run():
+    db_location, num_results_to_retrieve, collection_name = load_args()
+    client = chromadb.PersistentClient(path=db_location)
 
-    collection_name = "image_text_collection"
-    collection = get_collection(collection_name)
-
-    result = collection.query(
-        query_texts=["a machine learning engineer who is good at climbing"],
-        n_results=1
-    )
-
-    print(result)
-
+    while True:
+        question = input("Describe the image you are looking for: \n")
+        if question == "q":
+            break
+        
+        collection = get_collection(collection_name, client)
+        result = collection.query(
+            query_texts=[question],
+            n_results=num_results_to_retrieve
+        )
+        print(result["metadatas"])
+    
 
 
 if __name__ == "__main__":
-    main()
+    run()
