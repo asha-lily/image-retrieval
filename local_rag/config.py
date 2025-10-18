e81ed04060b8c3eec0f9502f2461fa73bd4486cd
asha-lily <ashalilypatel@gmail.com>
2025-10-18 16:49:28 +0100

    Update config.

--- local_rag/config.py
+++ local_rag/config.py
@@ -3,14 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     text_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/text/pizza_reviews.csv"
-    image_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/image_data_dict.csv"
+    image_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/image_data_dict_single_image.csv"
 
     vector_db_path: str = "./chroma_langchain_db"
-    collection_name: str = "pizza_reviews"
-    num_docs_to_retrieve: int = 5
-
-    embedding_model: str = "mxbai-embed-large"
-    chat_model: str = "llama3.2"
-    model_provider: str = "ollama"
-
-    basic_prompt: str = "You are a pizza connoisseur.\\Here are some reviews of pizza restaurants: {reviews}.\\Here is a question you need to answer: {question}"
+    image_collection_name: str = "image_text_collection"
+    num_results_to_retrieve: int = 3
