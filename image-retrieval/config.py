4dde790178a9b314f27cf4f3b9e89ca86d73644f
asha-lily <ashalilypatel@gmail.com>
2025-10-21 21:02:13 +0100

    Make vector DB path absolute so that DB gets persisted.

--- image-retrieval/config.py
+++ image-retrieval/config.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     text_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/text/pizza_reviews.csv"
-    image_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/image_data_dict_single_image.csv"
+    image_data_csv_path: str = "/Users/ashapatel/Documents/projects/local-rag/data/image_data_dict.csv"
 
-    vector_db_path: str = "./chroma_langchain_db"
+    vector_db_path: str = "/Users/ashapatel/Documents/projects/local-rag/chroma_langchain_db"
     image_collection_name: str = "image_text_collection"
     num_results_to_retrieve: int = 3
